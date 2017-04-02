9c067f284b96c6ca37617496c62d9bd6efcddaa7
Hossam Badawy <hossam.elsayed@student.guc.edu.eg>
2017-04-02 15:37:21 +0200

    First search draft

---
+++ controllers/controller.js
@@ -0,0 +1,230 @@
+let vSearch = require('../models/schema');
+
+
+function escapeRegex(text) = {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
+
+visitorSearch:function(req,res){
+		var noMatch=null;
+		const text = new RegExp(escapeRegex(req.query.search), 'gi');
+
+		if(req.query.criteria == "name"){
+
+			vSearch.find({"name":text},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					if(allItems.length < 1){
+						noMatch="No services match that criteria, please try again";
+					}
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+			})
+
+		} else {
+			if(req.query.criteria == "location"){
+
+			vSearch.find({"location":text},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					if(allItems.length < 1){
+						noMatch="No services match that criteria, please try again";
+					}
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+				}
+			})
+
+		} else {
+			if(req.query.criteria ==  "price"){
+
+			vSearch.find({"price":text},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					if(allItems.length < 1){
+						noMatch="No services match that criteria, please try again";
+					}
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+				}
+			})
+
+		} else {
+			if(req.query.criteria == "type"){
+
+			vSearch.find({"type":text},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					if(allItems.length < 1){
+						noMatch="No services match that criteria, please try again";
+					}
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+				}
+			})
+
+		} else {
+			if(!(req.query.search)){
+			vSearch.find({},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+			})
+		} }
+				} }}}
+
+
+clientSearch:function(req,res){
+		var noMatch=null;
+		const text = new RegExp(escapeRegex(req.query.search), 'gi');
+
+		if(req.query.criteria == "name"){
+
+			vSearch.find({"name":text},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					if(allItems.length < 1){
+						noMatch="No services match that criteria, please try again";
+					}
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+			})
+
+		} else {
+			if(req.query.criteria == "location"){
+
+			vSearch.find({"location":text},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					if(allItems.length < 1){
+						noMatch="No services match that criteria, please try again";
+					}
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+				}
+			})
+
+		} else {
+			if(req.query.criteria ==  "price"){
+
+			vSearch.find({"price":text},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					if(allItems.length < 1){
+						noMatch="No services match that criteria, please try again";
+					}
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+				}
+			})
+
+		} else {
+			if(req.query.criteria == "type"){
+
+			vSearch.find({"type":text},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					if(allItems.length < 1){
+						noMatch="No services match that criteria, please try again";
+					}
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+				}
+			})
+
+		} else {
+			if(!(req.query.search)){
+			vSearch.find({},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+			})
+		} }
+				} }}}
+
+adminSearch:function(req,res){
+		var noMatch=null;
+		const text = new RegExp(escapeRegex(req.query.search), 'gi');
+
+		if(req.query.criteria == "name"){
+
+			vSearch.find({"name":text},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					if(allItems.length < 1){
+						noMatch="No services match that criteria, please try again";
+					}
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+			})
+
+		} else {
+			if(req.query.criteria == "location"){
+
+			vSearch.find({"location":text},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					if(allItems.length < 1){
+						noMatch="No services match that criteria, please try again";
+					}
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+				}
+			})
+
+		} else {
+			if(req.query.criteria ==  "price"){
+
+			vSearch.find({"price":text},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					if(allItems.length < 1){
+						noMatch="No services match that criteria, please try again";
+					}
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+				}
+			})
+
+		} else {
+			if(req.query.criteria == "type"){
+
+			vSearch.find({"type":text},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					if(allItems.length < 1){
+						noMatch="No services match that criteria, please try again";
+					}
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+				}
+			})
+
+		} else {
+			if(!(req.query.search)){
+			vSearch.find({},function(err, allItems){
+				if(err){
+					console.log(err);
+				}else {
+					res.render(/*place here page name*/,{items:allItems, noMatch:noMatch})
+				}
+			})
+		} }
+				} }}}				
