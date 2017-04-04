d8b587cb9de1589d179a6f3402b1ae0f95073f40
Den <dapwner96@hotmail.com>
2017-04-04 18:34:43 +0200

    Added validPassword method to the client schema

--- app/models/client.js
+++ app/models/client.js
@@ -21,6 +21,10 @@ var ClientSchema = mongoose.Schema({
   recoveryToken: String //To be used for password recovery
 });
 
+ClientSchema.methods.validPassword = function(salt, password, hash) {
+ var enteredHash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex')
+ return enteredHash == hash
+}
 
 var Client = mongoose.model("client", ClientSchema);
 
