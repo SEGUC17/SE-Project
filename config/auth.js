9a2f8b24aad583c628b4d041a944c7a078b3fb51
Den <dapwner96@hotmail.com>
2017-04-04 17:26:47 +0200

    Added mailer authentication details to config/auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,12 @@
-module.exports={
+module.exports = {
 
-    'facebookAuth':{
+    facebookAuth:{
         clientID: 271754079904118,
         clientSecret: '05c2cb6700326d75f9c54b8829340cfc',
         callbackURL: 'http://localhost:8080/client/login/facebook/callback'
+    },
+    mailer: {
+      username: '3anateelse@gmail.com',
+      password: 'notsosecurepass'
     }
 };
