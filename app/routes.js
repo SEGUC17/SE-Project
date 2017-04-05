7e1b0e197dbbb3423c584ea370650b14daf74859
Karim156 <karim.elkhafif@student.guc.edu.eg>
2017-04-05 22:37:11 +0200

    Karim second commit

--- app/routes.js
+++ app/routes.js
@@ -5,13 +5,15 @@ router.post('/entertainment',z.add);
 router.get('/' ,z.get);
 router.post('/remove',z.remove);
 router.get('/admin',z.admin);
-
+router.get('/rate',z.rate);
 router.post('/editEmail',z.editEmail);
 router.post('/editPrice',z.editPrice);
 router.post('/editPhone',z.editPhone);
 router.post('/editLocation',z.editLocation);
 router.post('/editName',z.editName);
+router.post('/rateEntertainment',z.rateEntertainment);
 router.get('/edit',z.edit);
 
 
 module.exports = router;
+
